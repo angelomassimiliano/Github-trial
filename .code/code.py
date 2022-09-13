bc0a156c49632e4e3c4db43d60f1f5dcc741775d
angelomassimiliano <angelo.massimo666@gmail.com>
2022-09-13 16:34:56 +0200

    Added some code

--- .code/code.py
+++ .code/code.py
@@ -1,12 +1,11 @@
 import sys
-import numpy
+import numpy as np
 
 
 def main():
-    script = sys.argv[0]  # name of the script .py
-    filename = sys.argv[1]  # name of the file you want to read w/ the script
-    print(np.loadtxt(filename))
-
+    # script = sys.argv[0]  # name of the script .py
+    # filename = sys.argv[1]  # name of the file you want to read w/ the script
+    print(np.loadtxt(sys.stdin))
 
 if __name__ == '__main__':
     main()
