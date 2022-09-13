d55b7759768e58ee8840e48c58f035d604a02958
angelomassimiliano <angelo.massimo666@gmail.com>
2022-09-13 15:49:22 +0200

    Made code

--- .code/code.py
+++ .code/code.py
@@ -1,4 +1,13 @@
-import numpy as np
+import sys
+import numpy
 
-onesd = np.linspace(0, 1, 65)
-print(onesd)
+
+def main():
+    script = sys.argv[0]  # name of the script .py
+    filename = sys.argv[1]  # name of the file you want to read w/ the script
+    print(np.loadtxt(filename))
+
+
+if __name__ == '__main__':
+    main()
+
